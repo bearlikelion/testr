a5113405436daf39a4a570be274e5622f52c79f2
Mark Arneman <mark@arneman.me>
2020-10-28 19:46:31 -0400

    Config.yaml and CS object

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ Run pip install
 python -m venv venv
 pip install -r requirements.txt
 ```
+
+## Database Migrations
+We use SQLAlchemy to perform migrations on the database, to get it up and running execute the following commands:
+
+```
+flask db init
+flask db migrate
+flask db upgrade
+```
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+import yaml
+
+from cvpysdk.commcell import Commcell
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,5 +13,13 @@ app.secret_key = 'AJdfjasd8i0fawsjef'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+with open("config.yaml", 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        raise exc
+
+cs = Commcell(config['cshostname'], config['username'], config['password'])
 
 from app import routes, models
